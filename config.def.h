fd1303c945f566ea2eac6bcfbacec121dea1f5e3
Your Name <you@example.com>
2025-12-29 11:01:01 -0700

    update colors

--- config.def.h
+++ config.def.h
@@ -24,9 +24,9 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_lightpurple[]   = "#bd93f9";
+static const char col_lightpurple[]   = "#ff5555";
 static const char col_purple[]   = "#6d5890";
-static const char col_blue[]   = "#576695";
+static const char col_blue[]   = "#575d8e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
